4e6c4ac01ff81a3e87d5a960c060173d3740f361
Aleksandr Frolov <sasha-frolov@yandex-team.ru>
2019-09-08 14:31:32 +0300

    module4-task2 queries.sql add

--- data.php
+++ data.php
@@ -12,29 +12,29 @@ $tasks = [
 		'date_completed' => '25.12.2019',
 		'category' => 'Работа',
 		'executed' => false
-    ],
-    [
+	],
+	[
 		'title' => 'Сделать задание первого раздела',
 		'date_completed' => '21.12.2019',
 		'category' => 'Учеба',
 		'executed' => true
-    ],
-    [
+	],
+	[
 		'title' => 'Встреча с другом',
 		'date_completed' => '22.12.2019',
 		'category' => 'Входящие',
 		'executed' => false
-    ],
-    [
+	],
+	[
 		'title' => 'Купить корм для кота',
 		'date_completed' => null,
 		'category' => 'Домашние дела',
 		'executed' => false
-    ],
-    [
+	],
+	[
 		'title' => 'Заказать пиццу',
 		'date_completed' => null,
 		'category' => 'Домашние дела',
 		'executed' => false
-    ]
+	]
 ];
